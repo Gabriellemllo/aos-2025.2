e9d8a665f2fe120a1950f2d8d5d7acd8d3290ef9
Gabi Melo <gabriellesilvamelo622@gmail.com>
2025-08-19 23:43:46 +0000

    atv1

--- ex01-express/src/index.js
+++ ex01-express/src/index.js
@@ -1,7 +1,12 @@
 import "dotenv/config"
-
+import express from "express"
 require("dotenv").config();
 
-console.log("Olá mundo !");
+const app = express()
+const port = 3000
 
+console.log("!", process.env.MY_SECRET); 
 console.log(process.env.MY_SECRET);
+
+app.listen(3000);
+app.get("/", (req, res) => { res.send("Bem-vindo ao Express de Maria Gabrielle")})
